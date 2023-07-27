4e4cba90683017aa6b5eff64b332b67fc74e191d
Danii-Sh <83266030+Danii-Sh@users.noreply.github.com>
2023-07-27 16:30:21 +0330

    Add files via upload

---
+++ 1 client 3 servers/CA.B/kdc.c
@@ -0,0 +1,496 @@
+/* 
+ * echoserver.c - A simple connection-based echo server 
+ * usage: echoserver <port>
+ */
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/err.h>
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define BUFF_SIZE 1024
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int Crypt(int arc, char *argv[]);
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
+            int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv,
+            unsigned char *plaintext);
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
+            int aad_len, unsigned char *key, unsigned char *iv,
+            unsigned char *ciphertext, unsigned char *tag);
+void handleErrors(void);
+char tempplaintext[100];
+char tempkey[32];
+    /* Buffer for ciphertext. Ensure the buffer is long enough for the
+     * ciphertext which may be longer than the plaintext, dependant on the
+     * algorithm and mode
+     */
+    unsigned char ciphertext[128];
+
+time_t current_time;
+struct tm * time_info;
+char timeString[9];  // space for "HH:MM:SS\0"
+
+int main(int argc, char **argv) {
+  int listenfd; /* listening socket */
+  int connfd; /* connection socket */
+  int portno; /* port to listen on */
+
+  int tgs_lifetime; /*Time for tgs Ticket to Be Usable */
+  int v_lifetime; /*Time for V Ticket to Be Usable */
+  
+
+  int clientlen; /* byte size of client's address */
+  struct sockaddr_in serveraddr; /* server's addr */
+  struct sockaddr_in clientaddr; /* client addr */
+  struct hostent *hostp; /* client host info */
+  char buf[BUFF_SIZE]; /* message buffer */
+  char *hostaddrp; /* dotted decimal host addr string */
+  int optval; /* flag value for setsockopt */
+  int n; /* message byte size */
+
+  /* check command line args */
+  if (argc != 4) {
+    fprintf(stderr, "usage: %s <port> <TGS_LifeTime> <V_LifeTime>\n", argv[0]);
+    exit(1);
+  }
+  portno = atoi(argv[1]);
+  tgs_lifetime = atoi(argv[2]);
+  v_lifetime = atoi(argv[3]);
+
+  /* socket: create a socket */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0) 
+    error("ERROR opening socket");
+
+  /* setsockopt: Handy debugging trick that lets 
+   * us rerun the server immediately after we kill it; 
+   * otherwise we have to wait about 20 secs. 
+   * Eliminates "ERROR on binding: Address already in use" error. 
+   */
+  optval = 1;
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+	     (const void *)&optval , sizeof(int));
+
+  /* build the server's internet address */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET; /* we are using the Internet */
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); /* accept reqs to any IP addr */
+  serveraddr.sin_port = htons((unsigned short)portno); /* port to listen on */
+
+  /* bind: associate the listening socket with a port */
+  if (bind(listenfd, (struct sockaddr *) &serveraddr, 
+	   sizeof(serveraddr)) < 0) 
+    error("ERROR on binding");
+
+  /* listen: make it a listening socket ready to accept connection requests */
+  if (listen(listenfd, 5) < 0) /* allow 5 requests to queue up */ 
+    error("ERROR on listen");
+
+  /* main loop: wait for a connection request, echo input line, 
+     then close connection. */
+  clientlen = sizeof(clientaddr);
+  while (1) {
+
+    /* accept: wait for a connection request */
+    connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (connfd < 0) 
+      error("ERROR on accept");
+    
+    /* gethostbyaddr: determine who is connected */
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL)
+      error("ERROR on gethostbyaddr");
+    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL)
+      error("ERROR on inet_ntoa\n");
+    printf("server established connection with %s (%s)\n", 
+	   hostp->h_name, hostaddrp);
+ 
+
+    		n = write(connfd, "Insert User Name,an space then Server Number\n", 45);
+    		bzero(buf, BUFF_SIZE);
+    		n = read(connfd, buf, BUFF_SIZE);
+    		if (n < 0) 
+    		  error("ERROR reading from socket");
+    		printf("server received %d bytes: %s", n, buf);
+char buff=buf[n-2];
+if(buff=='0')
+{
+
+		/////********/////
+		FILE *outfile1;
+
+		char mystring[100];
+		int x=0;
+		while(x!=100)
+		{mystring[x]=NULL;
+		x++;}
+
+		outfile1 = fopen("user.txt","r");
+		int b=0;
+		while(b!=50)
+		{mystring[b]=fgetc (outfile1);
+		b++;} 
+		fclose(outfile1);
+	
+		int match=0;
+		x=0;
+		int y=1;
+		while(x!=n-4)
+		{if(mystring[x]!=buf[x])
+		{y=0;}
+		x++;}
+		int counter = n;
+
+	if(y==1)
+	{
+		x=0;
+		while(mystring[x]!=10)
+		{tempplaintext[x]=mystring[x];
+		x++;}
+		tempplaintext[x]=32;
+		x++;
+		while(mystring[x]!=10)
+		{tempplaintext[x]=mystring[x];
+		x++;}
+		tempplaintext[x]=32;
+		x++;
+
+		int k=0;
+		while(k!=20)
+		{tempkey[k]=mystring[k+x-1];
+		k++;}
+		k=0;
+		while(k!=12)
+		{tempkey[k+20]=mystring[k];
+		k++;}
+		//////*******//////
+		printf("User Found\n");
+
+		time(&current_time);
+		time_info = localtime(&current_time);
+
+		strftime(timeString, sizeof(timeString), "%H:%M:%S", time_info); 
+		k=0;
+		while(k!=sizeof(timeString))
+		{tempplaintext[k+x]=timeString[k];
+		k++;
+		} 
+		tempplaintext[k+x-1]=32;
+		tempplaintext[k+x]= tgs_lifetime;
+		
+		printf("tempplaintext: %s\n",tempplaintext);
+
+		Crypt(NULL, NULL);
+		    /* write: echo the input string back to the client */
+		    sprintf(buf,"%s\0", ciphertext);
+		    n = write(connfd, buf, strlen(buf));
+		    if (n < 0) 
+		      error("ERROR writing to socket");
+	}
+
+	printf("Out %d\n",n);
+
+    close(connfd);
+}
+
+if(buff=='1'||buff=='2'||buff=='3')
+{
+
+
+    		n = write(connfd, "T(tgs) needed to identify\n", 26);
+    		bzero(buf, BUFF_SIZE);
+    		n = read(connfd, buf, BUFF_SIZE);
+    		if (n < 0) 
+    		  error("ERROR reading from socket");
+
+    		if(buf[0]=='y')
+		{
+
+
+   ////READ TGS
+
+    n = write(connfd,"1 or 2 or 3\n", 12);
+    if (n < 0) 
+      error("ERROR writing to socket");
+		}
+
+
+
+    close(connfd);
+}
+
+
+
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int Crypt(int arc, char *argv[])
+{
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();     
+
+    /* Set up the key and iv. Do I need to say to not hard code these in a real application? :-) */
+
+    /* A 256 bit key */
+    static unsigned char key[] = "01234567890123456789012345678901";
+   int j=0;
+   while(j!=32)
+    {
+    key[j]=tempkey[j];
+    j++;
+    }
+
+    /* A 128 bit IV */
+    static unsigned char iv[] = "0123456789012345";
+
+    /* Message to be encrypted */
+    unsigned char plaintext[100]; //= "The quick brown fox jumps over the lazy dog";
+    j=0;
+    while(j!=100)
+    {
+    plaintext[j]=tempplaintext[j];
+    j++;    
+    }
+
+    /* Some additional data to be authenticated */
+    static unsigned char aad[] = "Some AAD data";
+
+
+
+    /* Buffer for the decrypted text */
+    unsigned char decryptedtext[128];
+
+    /* Buffer for the tag */
+    unsigned char tag[16];
+
+    int decryptedtext_len = 0, ciphertext_len = 0;
+
+    /* Encrypt the plaintext */
+    ciphertext_len = encrypt(plaintext, strlen(plaintext), aad, strlen(aad), key, iv, ciphertext, tag);
+
+    /* Do something useful with the ciphertext here */
+    printf("Ciphertext is:\n");
+    BIO_dump_fp(stdout, ciphertext, ciphertext_len);
+    printf("Tag is:\n");
+    BIO_dump_fp(stdout, tag, 14);
+
+    /* Mess with stuff */
+    /* ciphertext[0] ^= 1; */
+    /* tag[0] ^= 1; */
+
+    /* Decrypt the ciphertext */
+    decryptedtext_len = decrypt(ciphertext, ciphertext_len, aad, strlen(aad), tag, key, iv, decryptedtext);
+
+    if(decryptedtext_len < 0)
+    {
+        /* Verify error */
+        printf("Decrypted text failed to verify\n");
+    }
+    else
+    {
+        /* Add a NULL terminator. We are expecting printable text */
+        decryptedtext[decryptedtext_len] = '\0';
+
+        /* Show the decrypted text */
+        printf("Decrypted text is:\n");
+        printf("%s\n", decryptedtext);
+    }
+
+    /* Remove error strings */
+    ERR_free_strings();
+
+    return 0;
+}
+
+void handleErrors(void)
+{
+    unsigned long errCode;
+
+    printf("An error occurred\n");
+    while(errCode = ERR_get_error())
+    {
+        char *err = ERR_error_string(errCode, NULL);
+        printf("%s\n", err);
+    }
+    abort();
+}
+
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
+            int aad_len, unsigned char *key, unsigned char *iv,
+            unsigned char *ciphertext, unsigned char *tag)
+{
+    EVP_CIPHER_CTX *ctx = NULL;
+    int len = 0, ciphertext_len = 0;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+    /* Initialise the encryption operation. */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+        handleErrors();
+
+    /* Set IV length if default 12 bytes (96 bits) is not appropriate */
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
+        handleErrors();
+
+    /* Initialise key and IV */
+    if(1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+    /* Provide any AAD data. This can be called zero or more times as
+     * required
+     */
+    if(aad && aad_len > 0)
+    {
+        if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len))
+            handleErrors();
+    }
+
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(plaintext)
+    {
+        if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+            handleErrors();
+
+        ciphertext_len = len;
+    }
+
+    /* Finalise the encryption. Normally ciphertext bytes may be written at
+     * this stage, but this does not occur in GCM mode
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+    ciphertext_len += len;
+
+    /* Get the tag */
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag))
+        handleErrors();
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
+            int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv,
+            unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx = NULL;
+    int len = 0, plaintext_len = 0, ret;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+    /* Initialise the decryption operation. */
+    if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+        handleErrors();
+
+    /* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
+        handleErrors();
+
+    /* Initialise key and IV */
+    if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+    /* Provide any AAD data. This can be called zero or more times as
+     * required
+     */
+    if(aad && aad_len > 0)
+    {
+        if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
+            handleErrors();
+    }
+
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary
+     */
+    if(ciphertext)
+    {
+        if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+            handleErrors();
+
+        plaintext_len = len;
+    }
+
+    /* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag))
+        handleErrors();
+
+    /* Finalise the decryption. A positive return value indicates success,
+     * anything else is a failure - the plaintext is not trustworthy.
+     */
+    ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    if(ret > 0)
+    {
+        /* Success */
+        plaintext_len += len;
+        return plaintext_len;
+    }
+    else
+    {
+        /* Verify failed */
+        return -1;
+    }
+}
